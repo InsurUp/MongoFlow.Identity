4c854ff3b3678f653155308c65cb236f9457b4ff
kerem <kerem.acer@insurup.com>
2025-12-26 02:22:29 +0300

    refactor: simplify MongoUserToken class definition by removing redundant Id property

--- src/Core/Models/MongoUserToken.cs
+++ src/Core/Models/MongoUserToken.cs
@@ -5,8 +5,4 @@ namespace MongoFlow.Identity;
 
 public class MongoUserToken : MongoUserToken<ObjectId>;
 
-public class MongoUserToken<TKey> : IdentityUserToken<TKey> where TKey : IEquatable<TKey>
-{
-    public TKey Id { get; set; } = default!;
-}
-
+public class MongoUserToken<TKey> : IdentityUserToken<TKey> where TKey : IEquatable<TKey>;
