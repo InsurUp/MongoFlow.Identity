69c12870e0f068099d0e25cac8ebecbf273dbfb9
kerem-acer <76751305+kerem-acer@users.noreply.github.com>
2025-12-26 02:53:16 +0300

    feat(models): add Id property to MongoUserToken (#12)
    Add Id property to MongoUserToken<TKey> class to support MongoDB document identification, as the base IdentityUserToken<TKey> class doesn't include a primary key field.

--- src/Core/Models/MongoUserToken.cs
+++ src/Core/Models/MongoUserToken.cs
@@ -5,4 +5,7 @@ namespace MongoFlow.Identity;
 
 public class MongoUserToken : MongoUserToken<ObjectId>;
 
-public class MongoUserToken<TKey> : IdentityUserToken<TKey> where TKey : IEquatable<TKey>;
+public class MongoUserToken<TKey> : IdentityUserToken<TKey> where TKey : IEquatable<TKey>
+{
+    public TKey Id { get; set; } = default!;
+}
